6f6f33f619df4434098f4e95d5e29735ea94e740
Tiago Godinho <jtmgodinho@gmail.com>
2023-10-24 09:50:14 +0100

    Adding DNS and IP configuration script

--- SetupDomainController.ps1
+++ SetupDomainController.ps1
@@ -1,1 +1,14 @@
-#Configuration Variables
+# Install DNS Server role
+Install-WindowsFeature -Name DNS
+
+# Configure DNS settings
+$IPAddress = "10.0.0.2"
+$DnsServerAddress = "127.0.0.1"
+
+Set-DnsClientServerAddress -InterfaceAlias (Get-NetAdapter | Where-Object { $_.Status -eq 'Up' }).InterfaceAlias -ServerAddresses $DnsServerAddress
+
+# Configure static IP address
+New-NetIPAddress -IPAddress $IPAddress -PrefixLength 24 -InterfaceAlias (Get-NetAdapter | Where-Object { $_.Status -eq 'Up' })
+
+# Verify DNS configuration
+Resolve-DnsName $DomainName
