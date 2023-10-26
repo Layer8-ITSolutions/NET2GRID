643c108a68a79007b32c2dc26f51c902de5da15c
Leonardo <144500493+LeonardoPintoFigueira@users.noreply.github.com>
2023-10-26 16:28:41 +0100

    Update SetupDomainController.ps1

--- SetupDomainController.ps1
+++ SetupDomainController.ps1
@@ -1,21 +1,36 @@
-# Promote the server to a Domain Controller and create the forest
+# Ambient Variables
+# Configure DNS settings
+$IPAddress = "10.0.0.2"
+$DnsServerAddress = "127.0.0.1"
+# Set variables for forest and domain names
+$forestName = "NET2GRID.local"
+$domainName = "net2grid"
+
+# Configure static IP address
+New-NetIPAddress -IPAddress $IPAddress -PrefixLength 24 -InterfaceAlias (Get-NetAdapter | Where-Object { $_.Status -eq 'Up' })
+Set-DnsClientServerAddress -InterfaceAlias (Get-NetAdapter | Where-Object { $_.Status -eq 'Up' }).InterfaceAlias -ServerAddresses $DnsServerAddress
+
+# Rename the computer
+Rename-Computer -NewName "NET2GRID"
+
+#Reboot
+
+# Install Required Feature
+Install-WindowsFeature -Name DNS
 Install-WindowsFeature -Name AD-Domain-Services -IncludeManagementTools
-Install-ADDSForest -DomainName $domainName -DomainNetbiosName $domainName -ForestMode Win2016 -DomainMode Win2016 -InstallDns -Force -SafeModeAdministratorPassword $dsrmPassword
+
+
 
 # Import the Active Directory module
 Import-Module ActiveDirectory
 
-# Install DNS Server role
-Install-WindowsFeature -Name DNS
+# Promote the server to a Domain Controller and create the forest
+Install-ADDSForest -DomainName $domainName -DomainNetbiosName $domainName -ForestMode Win2016 -DomainMode Win2016 -InstallDns -Force -SafeModeAdministratorPassword $dsrmPassword
 
-# Configure DNS settings
-$IPAddress = "10.0.0.2"
-$DnsServerAddress = "127.0.0.1"
 
-Set-DnsClientServerAddress -InterfaceAlias (Get-NetAdapter | Where-Object { $_.Status -eq 'Up' }).InterfaceAlias -ServerAddresses $DnsServerAddress
 
-# Configure static IP address
-New-NetIPAddress -IPAddress $IPAddress -PrefixLength 24 -InterfaceAlias (Get-NetAdapter | Where-Object { $_.Status -eq 'Up' })
+
+
 
 # Verify DNS configuration
 Resolve-DnsName $DomainName
@@ -23,9 +38,7 @@ Resolve-DnsName $DomainName
 # Rename the computer
 Rename-Computer -NewName "NET2GRID"
 
-# Set variables for forest and domain names
-$forestName = "NET2GRID.local"
-$domainName = "net2grid"
+
 
 # Set the Directory Services Restore Mode (DSRM) password
 $dsrmPassword = ConvertTo-SecureString "DSRM_Password" -AsPlainText -Force
